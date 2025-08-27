2369152caf9e6bfdab80d21acc564f83fc56745a
pml-dy <pamelaverzonilla@gmail.com>
2025-08-27 20:54:20 +0000

    lab 1 exercise completed

--- lab1_verzonilla.cpp
+++ lab1_verzonilla.cpp
@@ -33,21 +33,25 @@ int main()
 
     cout<<"------ EXERCISE ------"<<endl;
     // declaring four variables
-    string country
-    char gender
-    int zipcode
-    float rate
+    string country;
+    char gender;
+    int zipcode;
+    float rate;
     // collect data
-    cout<<"Type a country:"<<endl;
+    cout<<"Type a country: ";
     cin>>country;
-    cout<<"What is your gender? (Use F/M)"<<endl;
+    cout<<"What is your gender? (Use F/M) ";
     cin>>gender;
-    cout<<"Zip code:"<<endl;
+    cout<<"What is your zipcode? ";
     cin>>zipcode;
-    cout<<"Tax rate:"<<endl;
+    cout<<"What is your tax rate? ";
     cin>>rate;
     // print the result
-    cout<<
+    cout<<"------ RESULTS ------"<<endl;
+    cout<<"Enter country:\t"<<country<<endl;
+    cout<<"Selected gender:\t"<<gender<<endl;
+    cout<<"Zip code:\t"<<zipcode<<endl;
+    cout<<"Tax rate:\t"<<rate<<endl;
 
     return 0;
 }
