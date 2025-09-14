af0307d010e4319f5b036ebc0e97fe9dd1d093d3
pml-dy <pamelaverzonilla@gmail.com>
2025-09-14 23:36:20 +0000

    act 1 step 4

---
+++ hw1_verzonilla/verzonilla.cpp
@@ -0,0 +1,31 @@
+/*
+Homework 1: Strings, String Methods, and Numerical Variables
+Pamela Day Verzonilla
+*/
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+// ACTIVITY 1
+
+int activity1() {
+    cout<<"------ ACTIVITY 1 ------"<<endl;
+    // step 1
+    string welcome = "Welcome to ";
+    string city = "New York City ";
+    // step 2
+    string name="";
+    cout<<"Please enter your name: ";
+    cin>>name;
+    // step 3
+    string concatenaded=welcome+city+name;
+    // step 4
+    cout<<concatenaded<<endl;
+    // step 5
+    // step 6
+    // step 7
+    // step 8
+
+    return 0;
+}
